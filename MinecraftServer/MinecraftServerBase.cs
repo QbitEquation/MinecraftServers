0272db6539e98e79ebc5556e90a8b7ae165b39b9
Tobias [PC] <itztobiias@gmail.com>
2021-06-06 20:07:03 +0200

    Addons can now auto-download required addons / programs

--- MinecraftServer/MinecraftServerBase.cs
+++ MinecraftServer/MinecraftServerBase.cs
@@ -1,6 +1,6 @@
 ﻿using ServerManagerFramework;
 
-[assembly: RequireProgram("Java")]
+[assembly: RequireProgram("Java", DownloadURL = "https://sdlc-esd.oracle.com/ESD6/JSCDL/jdk/8u291-b10/d7fc238d0cbf4b0dac67be84580cfb4b/JavaSetup8u291.exe?GroupName=JSC&FilePath=/ESD6/JSCDL/jdk/8u291-b10/d7fc238d0cbf4b0dac67be84580cfb4b/JavaSetup8u291.exe&BHost=javadl.sun.com&File=JavaSetup8u291.exe&AuthParam=1623003887_f422ec3982dc5a0213c5b798e8628f71&ext=.exe")]
 
 namespace MinecraftServer
 {
--- MinecraftVanilla/Vanilla.cs
+++ MinecraftVanilla/Vanilla.cs
@@ -1,7 +1,7 @@
 using ServerManagerFramework;
 using MinecraftServer;
 
-[assembly: RequireAddon("MinecraftServer")]
+[assembly: RequireAddon("MinecraftServer", DownloadURL = "https://github-releases.githubusercontent.com/374428709/0e608280-c702-11eb-94f1-53eca7b09128?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIWNJYAX4CSVEH53A%2F20210606%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20210606T180521Z&X-Amz-Expires=300&X-Amz-Signature=5c90d21205f089b1108e990ce2a0bd70dfdee53b196648d2a669a040a2b70944&X-Amz-SignedHeaders=host&actor_id=57042329&key_id=0&repo_id=374428709&response-content-disposition=attachment%3B%20filename%3DMinecraftServer.dll&response-content-type=application%2Foctet-stream")]
 
 namespace MinecraftVanilla
 {
